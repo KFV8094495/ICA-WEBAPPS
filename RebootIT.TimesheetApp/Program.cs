48340e4118ab136639f5e3ac72d5e388ebd97e0f
FALAKI <V8094495@live.tees.ac.uk>
2019-10-29 13:48:19 +0000

    Add project files.

---
+++ RebootIT.TimesheetApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RebootIT.TimesheetApp.Data;
+
+namespace RebootIT.TimesheetApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var env = services.GetRequiredService<IHostingEnvironment>();
+                if (env.IsDevelopment())
+                {
+                    var context = services.GetRequiredService<TimesheetDbContext>();
+                    context.Database.EnsureDeleted();
+                    context.Database.Migrate();
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
